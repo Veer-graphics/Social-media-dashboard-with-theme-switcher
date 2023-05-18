12c05ec37c2e3cc2ae7234875bf0e7dcf95b33b7
Veer-graphics <104774664+Veer-graphics@users.noreply.github.com>
2023-05-18 11:04:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const checkbox = document.getElementById('checkbox');
+const body = document.querySelector('body');
+checkbox.addEventListener('change', () => {
+    body.classList.toggle('dark');
+})
